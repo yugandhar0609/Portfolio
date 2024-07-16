06b9b5ba3d91270beffb1704d4021d6653d0c2ee
viejov3 <yugandharselvam@gmi=ail.com>
2024-07-16 21:08:43 +0530

    first commit

--- src/data/constants.js
+++ src/data/constants.js
@@ -134,7 +134,7 @@ export const skills = [
       {
         name: "Git",
         image:
-          "https://camo.githubusercontent.com/fbfcb9e3dc648adc93bef37c718db16c52f617ad055a26de6dc3c21865c3321d/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667",
+          "https://cdn.icon-icons.com/icons2/1381/PNG/512/git_93585.png",
       },
       {
         name: "GitHub",
